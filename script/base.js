d20e065350b5e5632d742088da1dbcdf73fc6e97
Brian <tzhlee@gmail.com>
2017-03-14 14:56:22 -0500

    Quote JsonP API Version

--- script/base.js
+++ script/base.js
@@ -7,4 +7,6 @@ $(function() {
          $(this).css('opacity', '1.0');
     });
 
+var quoteapi = "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=mycallback";
+
 });
