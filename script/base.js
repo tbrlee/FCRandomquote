d5c97ce1de1cefbbebfcf60a0c81a8f4a30093ec
Brian <tzhlee@gmail.com>
2017-03-14 09:24:14 -0500

    Jquery Button Syntax Fixed

---
+++ script/base.js
@@ -0,0 +1,10 @@
+$(function() {
+    $('#imgbutton').mousedown(function() {
+         $(this).css('opacity', '0.8');
+    });
+
+        $('#imgbutton').mouseup(function() {
+         $(this).css('opacity', '1.0');
+    });
+
+});
